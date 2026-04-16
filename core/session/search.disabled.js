099f22d54c914ce339acf547e6d16b05c7e215e7
Md. Fazley Rabbi <fazley111@gmail.com>
2026-04-16 10:53:16 +0600

    delete: unreferenced files

--- core/session/search.disabled.js
+++
@@ -1,8 +0,0 @@
-export function searchMessages(messages, query) {
-  if (!query) return messages;
-  const q = query.toLowerCase();
-
-  return messages.filter(m =>
-    m.content.toLowerCase().includes(q)
-  );
-}
